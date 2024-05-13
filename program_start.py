a7f4ff238ecc2fc105f79a075bd31d3702288a88
Fedor <fifero@mail.ru>
2024-05-13 19:27:45 +0300

    completed the translator added processing of string variables

--- program_start.py
+++ program_start.py
@@ -1,5 +1,6 @@
 from translator import from_language_to_machine_code
 if __name__ == '__main__':
-    f = open('test_1.txt', 'r')
+    f = open('tests/test_1.txt', 'r')
     program = f.read()
     from_language_to_machine_code(program)
+    f.close()
--- translator.py
+++ translator.py
@@ -1,12 +1,21 @@
 import re
 from isa import opcode
+
+def write_str_to_memory(str_data, machine_code, index):
+    for i in str_data:
+        machine_code += f"{{'index': {str(index)}, 'type': 'string', 'data': "
+        if i == "$":
+            machine_code += f"{0}}},\n"
+        else:
+            machine_code += f"{ord(i)}}},\n"
+        index += 1
+    return machine_code[:-3], index
+
 def from_language_to_machine_code(program: str):
     program = re.sub("\s{4}", "", program)
     data = re.split("\s", program)
 
-    if data[-1] != "halt":
-        raise Exception("Program without 'half'")
-
+    labels = {}
     machine_code = "["
     index = 0
 
@@ -24,14 +33,22 @@ def from_language_to_machine_code(program: str):
                         index += 1
                     else:
                         machine_code += ", 'arg': ["
+                elif ":" in data[j]:
+                    labels[data[j][:-1]] = index
                 else:
-                    new_el = re.sub(r'[,]', '', data[j])
-                    machine_code += f"'{new_el}'"
-                    if data[j + 1] in opcode:
+                    new_el = re.sub(r',', '', data[j])
+                    try:
+                        float(new_el)
+                        machine_code += f"{new_el}"
+                    except ValueError:
+                        machine_code += f"'{new_el}'"
+
+                    if data[j + 1] in opcode or ":" in data[j + 1]:
                         machine_code += "]},\n"
                         index += 1
                     else:
                         machine_code += ", "
+            break
 
     starting_data_index = index + 1
 
@@ -39,12 +56,18 @@ def from_language_to_machine_code(program: str):
     i = 1
     while True:
         index += 1
-        machine_code += f"{{'index': {str(index)}, 'data': "
         if data[i + 1] == "number":
-            machine_code += f"{str(data[i + 2])}"
+            machine_code += f"{{'index': {index}, 'type': 'number', 'data': "
+            machine_code += f"{data[i + 2]}"
         else:
-            # TODO
-            machine_code += f"'{data[i + 2]}'"
+            str_data = ""
+            while "$" not in data[i + 2]:
+                str_data += f"{data[i + 2]} "
+                i += 1
+            else:
+                str_data += f"{data[i + 2]}"
+
+            machine_code, index = write_str_to_memory(str_data, machine_code, index)
 
         if data[i + 3] == ".code:":
             machine_code += "}]"
@@ -59,6 +82,14 @@ def from_language_to_machine_code(program: str):
         old_index = re.search("\d{1,}", i).group(0)
         new_index = str(starting_data_index + int(old_index))
         machine_code = re.sub(f"'R{old_index}'", new_index, machine_code)
+
+    # Заменяем все лейблы на адреса команд
+    for i in list(labels.keys()):
+        machine_code = re.sub(f"'{i}'", str(labels[i]), machine_code)
+
+    f = open("machine_code.txt", "w+")
+    f.write(machine_code)
+    f.close()
 
     print(data)
     print(machine_code)
